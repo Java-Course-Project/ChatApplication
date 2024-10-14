4a227e101dc886f738a6da4fc36fa24d6071478b
duyvk <imkhuongduy@gmail.com>
2024-10-14 22:38:46 +0700

    fix bug for rate limiter

--- src/main/java/com/example/chatapplication/service/RateLimiterService.java
+++ src/main/java/com/example/chatapplication/service/RateLimiterService.java
@@ -3,11 +3,13 @@ package com.example.chatapplication.service;
 import com.example.chatapplication.exception.TooManyRequestException;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RateLimiterService {
 	private final DistributedLockService distributedLockService;
 	private final RedisTemplate<String, String> redisTemplate;
@@ -15,7 +17,7 @@ public class RateLimiterService {
 
 	// enum here for extension as we can have different timeframe and max token for different apis
 	public enum LimiterTokenType {
-		USER_REQUEST(Duration.ofSeconds(1), 1);
+		USER_REQUEST(Duration.ofSeconds(10), 1);
 		private final Duration timeFrame;
 		private final long max;
 
@@ -34,11 +36,13 @@ public class RateLimiterService {
 			Duration timeFrame = tokenType.timeFrame;
 			String fullKey = RATE_LIMITER_KEY + ":" + key;
 			if (redisTemplate.opsForValue().get(fullKey) == null) {
+				log.debug("Creating limiter key {}", fullKey);
 				redisTemplate.opsForValue().set(fullKey, "1", timeFrame);
 				return true;
 			}
 			Long currentToken = redisTemplate.opsForValue().increment(fullKey);
-			return currentToken != null && currentToken > tokenType.max;
+			log.debug("Current token {}", currentToken);
+			return currentToken != null && currentToken < tokenType.max;
 		} finally {
 			distributedLockService.unlock(key);
 		}
